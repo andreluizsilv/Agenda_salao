3b83686d638dfd597b7494a432c56ed446d8e857
andreluizsilv <109443784+andreluizsilv@users.noreply.github.com>
2025-12-29 21:25:56 -0300

    requeriment atualização settings.py

--- agenda_salao/settings.py
+++ agenda_salao/settings.py
@@ -22,13 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q^0%$u&ybh+ug%(cefypnycy=)b%&^^@%pxn&(^f69!-pxvlqp'
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-substitua-isso-em-producao")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["*"]
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
+
+ALLOWED_HOSTS = ["agendasalao-production-4c33.up.railway.app", "localhost", "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://agendasalao-production-4c33.up.railway.app"
+]
 
 # Application definition
 
